d7c680006f33bab1d3ee5233772be3ec792dde5e
Riataro <vishal.goddu@gmail.com>
2019-08-22 16:22:10 +0530

    Q5 changed

--- Assignment3/Code/Q5.tcl
+++ Assignment3/Code/Q5.tcl
@@ -1,42 +1,58 @@
 
+
 set ns [new Simulator]
 
-set nf [open q5.nam w]
-$ns namtrace-all $nf
+$ns color 0 Red
+$ns color 1 Green
+$ns color 2 Coral
+$ns color 3 Blue
+$ns color 4 Azure
+
+set f [open q5.nam w]
+$ns namtrace-all $f
 
 proc finish {} {
-    global ns nf
+    global ns f
     $ns flush-trace
-    close $nf
+    close $f
+    
     exec nam q5.nam &
     exit 0
 }
+puts "Enter no. of Nodes: "
+gets stdin N
+set n(0) [$ns node]
+set y "$n(0)"
+for {set i 1} {$i < $N} {incr i} {
+    set n($i) [$ns node]
+    append y " "
+    append y "$n($i)"
+}
+puts $y
+puts "$n(0) $n(1)"
+$ns make-lan $y 0.5Mb 40ms LL Queue/DropTail Mac/802_3
+puts "Enter k: "
+gets stdin k
+for {set i 0} {$i < $k} {incr i} {
+    gets stdin i1
+    gets stdin i2
+    set tcp [new Agent/TCP]
+    $tcp set class_ [expr $i%5]
+    $ns attach-agent $n($i1) $tcp
 
-set n0 [$ns node]
-set n1 [$ns node]
-set n2 [$ns node]
-set n3 [$ns node]
-set n4 [$ns node]
+    set sink [new Agent/TCPSink]
+    $ns attach-agent $n($i2) $sink
+    $ns connect $tcp $sink
+    $tcp set fid_ $i
 
-$ns make-lan "$n0 $n1 $n2 $n3 $n4" 0.5Mb 40ms LL Queue/DropTail Mac/802_3
-
-set tcp0 [new Agent/TCP]
-$tcp0 set class_ 1
-$ns attach-agent $n1 $tcp0
-
-set sink0 [new Agent/TCPSink]
-$ns attach-agent $n3 $sink0
-$ns connect $tcp0 $sink0
-
-set cbr0 [new Application/Traffic/CBR]
-$cbr0 set packetSize_ 500
-$cbr0 set interval_ 0.01
-$cbr0 attach-agent $tcp0
-
-$ns at 0.5 "$cbr0 start"
-$ns at 4.5 "$cbr0 stop"
-
-$ns at 5.0 "finish"
-
+    set ftp($i) [new Application/FTP]
+    $ftp($i) attach-agent $tcp
+    $ftp($i) set type_ FTP
+}
+for {set i 0} {$i < $k} {incr i} {
+    $ns at [expr ($i/10)+0.1] "$ftp($i) start"
+    $ns at [expr ($i/10)+1.5] "$ftp($i) stop"
+}
+$ns at [expr ($k/10)+1.5] "finish"
 
 $ns run
